558f2922a4f7bf912659e9d9df2868a8af5154d4
AnaPanamito <ana.panamito@unl.edu.ec>
2024-11-29 12:33:21 -0500

    Update Notificacion.java

--- Notificacion.java
+++ Notificacion.java
@@ -1,14 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package gestion.de.eventos;
-
-/**
- *
- * @author InnoVausuario
- */
-public interface Notificacion {
-  void actualizarParticipantes();
-
+interface Notificacion {
+    void actualizarParticipantes(String mensaje);
 }
